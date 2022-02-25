babe6090db0bd04e317ac06219f27d1d6d16440c
capchapez <37667273+capchapez@users.noreply.github.com>
2022-02-25 15:15:42 -0500

    Update prueba.java

--- prueba.java
+++ prueba.java
@@ -1,8 +1,46 @@
+import java.util.Scanner;
+
 public class Main{
 	public static void main(String[] args) {
-		Persona myObj = new Persona();
-		System.out.println(myObj.nombre);
+		Registro obR = new Registro();
+		obR.realizar();
 	}
+}
+
+abstract class Registro{
+	public void realizar() {
+		Formulario obF = Formulario();
+		obF.preguntar();
+		Repuesta obR = Repuesta();
+		obR.mostrar();
+	}
+}
+
+public class Formulario{
+	public preguntar(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ingresa Nombre de Usuario");
+		String strNombre = sc.nextLine();
+		
+		System.out.println("Ingresa Edad de Usuario");
+		String strEdad = sc.nextLine();
+		
+		System.out.println("Ingresa Telefono de Usuario");
+		int intTelefono = sc.nextLine();
+		Persona obP = Persona();
+		obP.persona(strNombre, strEdad, intTelefono);
+	}
+}
+
+public class Repuesta{
+	public mostrar(){
+		Persona obP = Persona();
+		System.out.println("Datos de Registro:");
+		System.out.println(obP.setNombre);
+		System.out.println(obP.setEdad);
+		System.out.println(obP.setTelefono);
+	}
+
 }
 
 public class Persona{
@@ -10,6 +48,12 @@ public class Persona{
 	private String edad;
 	private int telefono;
 
+	public persona(String strNombre, String strEdad, int intTelefono){
+		nombre = strNombre.getNombre;
+		edad = strEdad.getEdad;
+		telefono = intTelefono.getTelefono;
+	}
+	
 	public String getNombre() {
 		return nombre;
 	}
