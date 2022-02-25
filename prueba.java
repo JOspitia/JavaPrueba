9bc48aef12990d68c40b025631d5b5eec2bd527e
capchapez <37667273+capchapez@users.noreply.github.com>
2022-02-25 15:00:13 -0500

    Update prueba.java

--- prueba.java
+++ prueba.java
@@ -1,37 +1,36 @@
 public class Main{
 	public static void main(String[] args) {
-    Persona myObj = new Persona();
-    		System.out.println(myObj.nombre);
-  	}
-
+		Persona myObj = new Persona();
+		System.out.println(myObj.nombre);
+	}
 }
 
 public class Persona{
-	  private String nombre;
-	  private String edad;
-	  private int telefono;
+	private String nombre;
+	private String edad;
+	private int telefono;
 
-	  public String getNombre() {
-      return nombre;
-  	}
+	public String getNombre() {
+		return nombre;
+	}
 
-  	public void setNombre(String newNombre) {
-      this.nombre = newNombre;
-  	}
+	public void setNombre(String newNombre) {
+		this.nombre = newNombre;
+	}
 
-	  public String getEdad() {
-    	return edad;
-  	}
+	public String getEdad() {
+		return edad;
+	}
 
-  	public void setEdad(String newEdad) {
-      this.edad = newEdad;
-  	}
+	public void setEdad(String newEdad) {
+		this.edad = newEdad;
+	}
 
-	  public String getTelefono() {
-      return telefono;
-  	}
+	public String getTelefono() {
+		return telefono;
+	}
 
-  	public void setTelefono(String newTelefono) {
-      this.telefono = newTelefono;
-  	}
+	public void setTelefono(String newTelefono) {
+		this.telefono = newTelefono;
+	}
 }
